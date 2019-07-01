0c328bd330368f21950269f7848737b00ea10144
SajeSkript <sajeskript@gmail.com>
2019-07-01 21:12:01 +0000

    first running copy of test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='sr!')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.command()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+bot.run('NTk1MjY1MjE5NTE3NzQzMTE1.XRpAtw.1sgSY03oWZORy34Qmogug5Aym3s')
