2a172b2af77ecb2a80bbdfb96ae08ba46c08dc6f
SajeSkript <sajeskript@gmail.com>
2019-07-03 19:51:42 +0000

    Added logging to the script. It will dump to the logging.debug file. Also changed the name of cred.py to custvar.py and updated in the templates folder and main.py script. Added new variable to custvar.py file called prefix to allow one centralized location for customization to a small degree

---
+++ custvar.py
@@ -0,0 +1,7 @@
+#This is the Bot Token you will recieve from the Discord Developer 
+#Dashboard when you add a bot to your application
+api_token = "NTk1MjY1MjE5NTE3NzQzMTE1.XRtzng.JJpRS2YZAvzDnf2nltVX0xe5EsY"
+
+#This is the command prefix that users will type in discord to
+#interact with this bot
+prefix = "sr!"
--- main.py
+++ main.py
@@ -1,19 +1,49 @@
-#Import credentials
-import creds
+#################################################
+#Importing Libraries, Variables, Modules, ect...
+#################################################
+
+#Import logging capabilities.
+import logging
+
+#Import custom variables from custvar.py file
+import custvar
 
 #Import commands from Discord library
 import discord
 from discord.ext import commands
 
 
-bot = commands.Bot(command_prefix='sr!')
 
+#################################################
+#Logging
+#
+#logging levels: debug, info, warning, error, and critical
+#################################################
+logging.basicConfig(level=logging.INFO, filename='logging.debug', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#logging.debug('This will get logged to a file')
+
+
+
+#################################################
+#Where the prefix for the bot it set.
+#################################################
+bot = commands.Bot(command_prefix=custvar.prefix)
+#################################################
+#The main functions of the bot
+#################################################
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    print("Everything's all ready to go~")
+
+
+@bot.event
+async def on_message(message):
+    print("The message's content was", message.content)
+
 
 @bot.command()
 async def add(ctx, a: int, b: int):
@@ -35,4 +65,5 @@ async def cj(ctx):
 async def cat(ctx):
     await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
-bot.run(creds.api_token)
+bot.run(custvar.api_token)
+logging.error(f'{custvar.api_token} raised an error')
