f91f3b87ce67917129fc326d5c91c9ddc7123f89
SajeSkript <sajeskript@gmail.com>
2019-07-05 20:14:33 +0000

    Added functionality to export server user list. Need to further expand on the information being exported, such as full user id so that users can be re-invited should disaster strike

--- main.py
+++ main.py
@@ -5,27 +5,35 @@
 #Import custom variables from custvar.py file
 import custvar
 
+#Import other Libraries ect..
+import csv
+import time
+import random
+import logging
+import tempfile
+
 #Import commands from Discord library
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
+#from discord.ext.commands import Bot
 
-#Import random
-import random
 
-#Import logging capabilities.
-#import logging
 
 #################################################
 #Logging
-#
 #logging levels: debug, info, warning, error, and critical
 #################################################
 #logging.basicConfig(level=logging.INFO, filename='/var/log/sagerecovery/logging.debug', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
 
 #################################################
 #Where the prefix for the bot it set.
 #################################################
 bot = commands.Bot(command_prefix=custvar.prefix)
+
+
 
 #################################################
 #The main functions of the bot
@@ -37,19 +45,12 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-#@bot.event
-#async def on_message(message):
-    #print("The message's content was", message.content)
 
 
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
-
-@bot.command()
-async def cj(ctx):
-    await ctx.send(":smiley: Fuck Kaiwolf! :wink:")
-
+#################################################
+#On Message section. All on_message statements 
+# must fall under this one section.
+#################################################
 @bot.event
 async def on_message(message):
      if message.author != bot.user:
@@ -73,5 +74,50 @@ async def on_message(message):
 
 
      await bot.process_commands(message)
+#----------------------------------------------
+#end On message section. Do not put any 
+#on_message code below await bot.process 
+#command. 
+#----------------------------------------------
+
+
+
+###############################################
+#Begin Bot commands section.
+###############################################
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.command()
+async def cj(ctx):
+    await ctx.send(":smiley: Fuck Kaiwolf! :wink:")
+
+#Section used to get user list from server and
+#export to excel spreedsheet
+@bot.command(pass_context=True)
+async def userlist(ctx):
+    """Returns a CSV file of all users on the server."""
+    if ctx.guild.large == 'true':
+        await bot.request_offline_members(ctx.guild.members)
+    before = time.time()
+    nicknames = [m.display_name for m in ctx.guild.members]
+    tf = tempfile.NamedTemporaryFile(suffix='.csv')
+    with open(tf.name, mode='w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f, dialect='excel')
+        for v in nicknames:
+            writer.writerow([v])
+    after = time.time()
+    await ctx.message.author.send(content=(f'Please see the attached user list for your server: {ctx.guild}'), file=discord.File(tf.name))
+
+
+
+#----------------------------------------------
+#end Bot commands  section.
+#----------------------------------------------
+
+
+
+
 if __name__ == '__main__':
      bot.run(custvar.api_token)
